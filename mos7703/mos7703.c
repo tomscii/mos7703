9d1e4a0e811c415b9cb129eca633775894f29722
Tom Szilagyi <tomszilagyi@gmail.com>
2014-05-14 20:15:03 +0200

    Remove obsoleted code

--- mos7703/mos7703.c
+++ mos7703/mos7703.c
@@ -269,22 +269,9 @@ static void mos7703_bulk_out_data_callback(struct urb *urb)
 	}
 
 	tty = mos7703_port->tty;
-#if 1
 	if (mos7703_port->open) {
 		tty_port_tty_wakeup(tty->port);
 	}
-#else
-	if (tty) {
-		/* let the tty driver wakeup if it has a special write_wakeup function. */
-		if ((tty->flags & (1 << TTY_DO_WRITE_WAKEUP)) &&
-		    tty->ldisc->ops->write_wakeup) {
-			(tty->ldisc->ops->write_wakeup)(tty);
-		}
-
-		/* tell the tty driver that something has changed */
-		wake_up_interruptible(&tty->write_wait);
-	}
-#endif
 }
 
 /*****************************************************************************
